f4026a08a5eb60b491d5f336d9fbf6476616aa84
Gabryel <gabryel.ferreira@iteris.com.br>
2020-05-11 00:01:13 -0300

    Initial commit

---
+++ src/db/createConn.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import path from 'path';
+
+export async function createConn() {
+    return new Promise((resolve, reject) => {
+        createConnection({
+            type: "sqlite",
+            database: "express.db",
+            entities: [
+                path.resolve(__dirname, "..", "entity") + "/*.ts"
+            ],
+            migrationsTableName: "custom_migration",
+            migrations: ["./migrations/*.ts"],
+            cli: {
+                migrationsDir: "./migrations"
+            }
+        })
+            .then(() => {
+                console.log("Successfully connected to database");
+                resolve();
+            })
+            .catch((e) => {
+                console.log("Error connection to database", e);
+                reject(e);
+            });
+    })
+}
